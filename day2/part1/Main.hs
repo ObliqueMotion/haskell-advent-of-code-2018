822850072213a6c2f1b9eeb26f56aeaddf4730da
Erik Nordin <aeketn@gmail.com>
2018-12-01 23:10:59 -0800

    Solved Day 2 Problem 1

---
+++ day2/part1/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Data.List
+
+contains :: Int -> String -> Bool
+contains frequency = any (== frequency) . map length . group . sort
+
+main :: IO ()
+main = do
+    input <- (readFile "files/input")
+    let ids = lines input
+    let pair = 2
+    let triple = 3
+    print $ (length $ filter (contains pair) ids) * (length $ filter (contains triple) ids)
