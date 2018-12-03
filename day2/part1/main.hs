ba7612a752a838b953915c95d363ef8678737dbc
Erik Nordin <aeketn@gmail.com>
2018-12-03 14:20:42 -0800

    Simplified Cabal Files

--- day2/part1/main.hs
+++ day2/part1/main.hs
@@ -2,12 +2,12 @@ module Main where
 import Data.List
 
 contains :: Int -> String -> Bool
-contains frequency = any (== frequency) . map length . group . sort
+contains letterFrequency = any (== letterFrequency) . map length . group . sort
 
 main :: IO ()
 main = do
     input <- (readFile "../input")
     let ids = lines input
-    let pair = 2
-    let triple = 3
-    print $ (length $ filter (contains pair) ids) * (length $ filter (contains triple) ids)
+    let numIdsWithPairs   = length $ filter (contains 2) ids
+    let numIdsWithTriples = length $ filter (contains 3) ids
+    print (numIdsWithPairs * numIdsWithTriples)
