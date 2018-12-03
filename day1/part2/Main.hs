b01ebc5718f9d03c5f8a37899b88d96a2e895c85
Erik Nordin <aeketn@gmail.com>
2018-12-03 13:00:41 -0800

    Refactored Day1 Part2

--- day1/part2/Main.hs
+++ day1/part2/Main.hs
@@ -6,19 +6,16 @@ parseInt :: String -> Int
 parseInt ('+':digits) = read digits :: Int
 parseInt x = read x :: Int
 
-firstReachedTwice :: (Maybe Int, HashSet Int, Int) -> [Int] -> Int
-firstReachedTwice (Just answer,_,_) _ = answer
-firstReachedTwice (Nothing,seen,current) changeList = firstReachedTwice (scan changeList seen current) changeList
-    where 
-    scan [] seen current = (Nothing, seen, current)
-    scan (change:changeList) seen current
-        | (member newFrequency seen) = (Just newFrequency,seen,current)
-        | otherwise = scan changeList (insert newFrequency seen) newFrequency
+firstReachedTwice :: [Int] -> Int
+firstReachedTwice changeList = scan changeList (fromList [0]) 0
+    where
+    scan (change:changes) seen current
+        | (newFrequency `member` seen) = newFrequency
+        | otherwise = scan changes (insert newFrequency seen) newFrequency
         where newFrequency = current+change
 
 main :: IO ()
 main = do
     input <- (readFile "files/input")
     let changeList = Prelude.map parseInt $ lines input
-    let hashSet = fromList [0]
-    print $ firstReachedTwice (Nothing, hashSet, 0) changeList
+    print $ firstReachedTwice (cycle changeList)
