88d7b2ae5d43307dc62fab5d5ca6446d7df0518c
Erik Nordin <aeketn@gmail.com>
2018-12-03 11:27:18 -0800

    Solved Day3 Part1

---
+++ day3/part1/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+-- Exapmle ["123", "987"] -> (123,987)
+toTuple :: [String] -> (Int,Int)
+toTuple [x,y] = ((read x::Int),(read y::Int))
+
+-- Example "123,987:" -> (123,987)
+parseStartIndex :: [String] -> (Int,Int)
+parseStartIndex (_:_:index:_) = toTuple $ splitOn "," $ index \\ ":"
+
+-- Example "123x987" -> (123,987)
+parseDimensions :: [String] -> (Int,Int)
+parseDimensions (_:_:_:dimensions:_) = toTuple $ splitOn "x" dimensions
+
+-- Example ["#1","@","49,222:","19x20"] -> [(49,222), (19,20)]
+parseClaim :: [String] -> [(Int,Int)]
+parseClaim claim = [parseStartIndex claim, parseDimensions claim]
+
+-- Example: [(2, 5), (2,3)] -> [(3,6), (4,6), (3,7), (4,7), (3,8), (4,8)]
+expandClaim :: [(Int,Int)] -> [(Int,Int)]
+expandClaim [] = []
+expandClaim [(xStart,yStart),(xOffset,yOffset)] = [(x,y) | x <- xs, y <- ys]
+    where
+    xs = map (+xStart) [1..xOffset]
+    ys = map (+yStart) [1..yOffset]
+
+main :: IO ()
+main = do
+    input <- (readFile "files/input")
+    let claims = concat $ map expandClaim $ map parseClaim $ map words $ lines input
+    let overLappingSquares = filter ((>1) . length) $ group $ sort claims
+    print $ length overLappingSquares
