ef3481d61b13b8a92569c3d86b559240354481ab
Erik Nordin <aeketn@gmail.com>
2018-12-05 19:46:02 -0800

    Refactored (Part1,Part2)

--- day5/part1/main.hs
+++ day5/part1/main.hs
@@ -1,24 +1,17 @@
 module Main where
 import Data.Char
 
-data Result = Reducible | MaybeReducible | Irreducible deriving(Eq)
-
-react :: (String,Result) -> (String,Result)
-react ((x:y:ys),result)
-    | x `reactsWith` y = react (ys,Reducible)
-    | otherwise = (x:(fst reacted),snd reacted)
-    where reacted = react ((y:ys),result)
-          reactsWith x y = (toUpper x) == (toUpper y) && x /= y
-react (end,result)
-    | result == Reducible      = (end,MaybeReducible)
-    | result == MaybeReducible = (end,Irreducible)
+reactsWith :: Char -> Char -> Bool
+reactsWith x y = (toUpper x == toUpper y) && x /= y
 
 fullyReact :: String -> String
-fullyReact polymer = fst $ head $ dropWhile (reducible) $ iterate react (polymer,MaybeReducible)
-    where reducible = (==MaybeReducible) . snd
+fullyReact = foldr react []
+  where react x (y:ys)
+            | x `reactsWith` y = ys
+            | otherwise = (x:y:ys)
+        react x [] = [x]
 
 main :: IO ()
 main = do
     polymer <- (readFile "../input")
-    print $ length polymer
     print $ length $ fullyReact polymer
