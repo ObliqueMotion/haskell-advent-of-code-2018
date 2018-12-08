512ff299d3124ee9bfd5af0c45fe5f9ad61f15c9
Erik Nordin <aeketn@gmail.com>
2018-12-08 14:43:17 -0800

    Renamed Types For Readability

--- day6/part2/main.hs
+++ day6/part2/main.hs
@@ -1,15 +1,17 @@
 module Main where
 import Data.List
 
+type Point = (Int,Int)
+
 -- Example: "152, 25" -> (152,25)
-parsePoint :: String -> (Int,Int)
+parsePoint :: String -> Point
 parsePoint s = (x,y)
     where nums = words $ s \\ ","
           x = read (head nums) :: Int
           y = read (head $ tail nums) :: Int
         
 -- Returns the samllest rectangle that contains a set of 2D points.
-gridContainingAll :: [(Int,Int)] -> [(Int,Int)]
+gridContainingAll :: [Point] -> [Point]
 gridContainingAll points = [(x,y) | x <- [xMin..xMax], y <- [yMin..yMax]]
     where xMin = minimum xs
           xMax = maximum xs
@@ -19,12 +21,12 @@ gridContainingAll points = [(x,y) | x <- [xMin..xMax], y <- [yMin..yMax]]
           ys   = map snd points
 
 -- Returns the Manhattan Distance between two 2D points.
-distanceFrom :: (Int,Int) -> (Int,Int) -> Int
+distanceFrom :: Point -> Point -> Int
 distanceFrom (x1,y1) (x2,y2) = (abs(x1-x2) + abs(y1-y2))
 
 -- Returns the a set containing points that are closest to a target point.
 -- If two or more points are equally close, they will all be in the set.
-sumDistanceFromAll :: [(Int,Int)] -> (Int,Int) -> Int
+sumDistanceFromAll :: [Point] -> Point -> Int
 sumDistanceFromAll points target = sum $ map (distanceFrom target) points
 
 main :: IO ()
